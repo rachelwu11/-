966c1b570d1d47da4c240d161fe6cac69a3f93ed
rachelwu11 <xianw5@gmail.com>
2017-03-26 23:42:24 -0400

    init repo

---
+++ PokemonCome/PokemonCome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonCome
+//
+//  Created by rachel on 3/26/17.
+//  Copyright © 2017 rwu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
