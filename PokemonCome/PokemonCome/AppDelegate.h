d67c333633ac36466f92fc26011ff5ab04b55abe
rachelwu11 <xianw5@gmail.com>
2017-03-29 08:26:33 -0400

    rewrite in objective C, during swift compile issue

---
+++ PokemonCome/PokemonCome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PokemonCome
+//
+//  Created by rachel on 3/29/17.
+//  Copyright © 2017 rwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
